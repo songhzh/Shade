bc86ebe44a8dd18c00a4495b1fc40f91c1481652
Phlana <joshua.chang422@gmail.com>
2019-01-19 14:35:37 -0700

    changed rgb to hex code

--- encrypt.py
+++ encrypt.py
@@ -6,5 +6,6 @@ img.show()
 
 for i in range(200):
     for j in range(200):
-        r, g, b = pix[i, j]
-        print(r, g, b)
+        # r, g, b = pix[i, j]
+        # print(r, g, b)
+        print('%02x%02x%02x' % pix[i, j])
