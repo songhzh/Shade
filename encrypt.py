ba97dd0480c54a2fdf43cb20ccf0db084b67bdfe
Song Hui Zhang <songhui@ualberta.ca>
2019-01-19 16:01:04 -0700

    maid service

--- encrypt.py
+++ encrypt.py
@@ -15,54 +15,66 @@ from PIL import Image
 
 
 class Pixels:
-    def __init__(self):
-        # images
-        self.key = Image.open('images/kon.jpg').convert("RGB").load()
-        self.img_1 = Image.open('images/rnm.png').convert("RGB").load()
-        self.img_2 = Image.open('images/homer.jpg').convert("RGB").load()
+    @staticmethod
+    def open(link):
+        return Image.open(link).convert("RGB").load()
 
-    def encode(self, image1, image2):
+    @staticmethod
+    def xor_list(image1, image2):
         rgb_list = []
-        for i in range(200):
-            for j in range(200):
-                keyR, keyG, keyB = image1[j, i]
-                r, g, b = image2[j, i]
-                print("(", keyR, keyG, keyB, ")", "or", "(", r, g, b, ")")
-                mix = (keyR ^ r, keyG ^ g, keyB ^ b)
-                print(mix)
+
+        for y in range(200):
+            for x in range(200):
+                r1, g1, b1 = image1[x, y]
+                r2, g2, b2 = image2[x, y]
+                # print("(", keyR, keyG, keyB, ")", "or", "(", r, g, b, ")")
+                mix = (r1 ^ r2, g1 ^ g2, b1 ^ b2)
+                # print(mix)
                 rgb_list.append(mix)
 
         return rgb_list
 
-    def create_image(self, rgb_list):
+    @staticmethod
+    def create_image(rgb_list):
         out = Image.new("RGB", (200, 200))
         out.putdata(rgb_list)
-        out.save("images/out.png")
-        # out.show()
+        # out.save("images/out.png")
         return out
 
-    def show_image(self, image):
-        image.show()
+    @staticmethod
+    def encode(image1, image2):
+        return Pixels.create_image(Pixels.xor_list(image1, image2))
+
+    @staticmethod
+    def display(image1, image2, load=False):
+        img = Pixels.encode(image1, image2)
+        img.show()
+        return img.load() if load else img
 
 
 if __name__ == "__main__":
-    pix = Pixels()
-    list_1 = pix.encode(pix.key, pix.img_1)
-    mid = pix.create_image(list_1)
+    kon = Pixels.open('images/kon.jpg')
+    rnm = Pixels.open('images/rnm.png')
+    homer = Pixels.open('images/homer.jpg')
+    kon_rnm = Pixels.display(kon, rnm, True)
+    kon_rnm_homer = Pixels.display(kon_rnm, homer)
 
-    pix.show_image(mid)
-
-    list_2 = pix.encode(mid.load(), pix.img_2)
-    out = pix.create_image(list_2)
-
-    pix.show_image(out)
-
-    list_3 = pix.encode(out.load(), pix.img_1)
-    out_1 = pix.create_image(list_3)
-
-    pix.show_image(out_1)
-
-    list_4 = pix.encode(out_1.load(), pix.key)
-    out_2 = pix.create_image(list_4)
-
-    pix.show_image(out_2)
+    # list_1 = pix.encode(pix.key, pix.img_1)
+    # mid = pix.create_image(list_1)
+    #
+    # pix.show_image(mid)
+    #
+    # list_2 = pix.encode(mid.load(), pix.img_2)
+    # out = pix.create_image(list_2)
+    #
+    # pix.show_image(out)
+    #
+    # list_3 = pix.encode(out.load(), pix.img_1)
+    # out_1 = pix.create_image(list_3)
+    #
+    # pix.show_image(out_1)
+    #
+    # list_4 = pix.encode(out_1.load(), pix.key)
+    # out_2 = pix.create_image(list_4)
+    #
+    # pix.show_image(out_2)
