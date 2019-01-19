f5c49b2180d6e8248d6d1bb55996697459f17cf0
Song Hui Zhang <songhui@ualberta.ca>
2019-01-19 14:30:27 -0700

    add rick and morty

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,10 @@
 from PIL import Image
 
-img = Image.open('images/kon.jpg').convert("RGB")
-pixels = img.load()
+img = Image.open('images/rnm.png').convert("RGB")
+pix = img.load()
+img.show()
 
-print(img)
+for i in range(200):
+    for j in range(200):
+        r, g, b = pix[i, j]
+        print(r, g, b)
