9c0cedf50991078acca080b6b76c228565ddcfbc
thebromoe <kebbi@ualberta.ca>
2019-01-19 17:57:02 -0700

    add encryption class

---
+++ encryption.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import os
+
+
+class Encryption:
+    def __init__(self, file_to_encrypt):
+        self.file_to_encrypt = file_to_encrypt
+        self.image_info = dict()
+
+    def get_file_type(self, file):
+        return file.split('.')[-1]
